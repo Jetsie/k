0bd7a6bf5b7b28c9188449e3520636a74ca31e87
Jet <84949210+Jetsie@users.noreply.github.com>
2021-09-03 23:08:27 -1000

    Moved to heroku

--- project/urls.py
+++ project/urls.py
@@ -2,13 +2,16 @@ from django.urls import path, include
 
 from django.contrib import admin
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
 import content.views
 
 
 urlpatterns = [
-    path("", content.views.index, name="index"),
-    path("db/", content.views.db, name="db"),
+    path("", content.views.index, name="index.html"),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
