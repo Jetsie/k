6454e7cad9f5256d49fb6deb69ae339897caf5e9
Jet <84949210+Jetsie@users.noreply.github.com>
2021-09-03 20:26:50 -1000

    Moved to heroku

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,7 @@ import content.views
 
 
 urlpatterns = [
-    path("", content.views.home, name="home"),
+    path("", content.views.home, name="index"),
     path("db/", content.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
